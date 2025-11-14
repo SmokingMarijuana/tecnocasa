b5c326e3f208a77bd72eb81970a4c2e5908211dd
SmokingMarijuana <kebabrumeno3euro@gmail.com>
2025-11-14 09:23:39 +0100

    First commit, with the Hero component rendered

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from './components/Hero'
+import './components/Hero.css'
+function App() {
+  return (
+    <>
+      <Hero />
+
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Menu.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Menu.css"; // Importiamo solo il CSS della hero
+
+const Hero = () => {
+  return (
+    <section className="hero">
+    
+      <header id="header">
+        <a id="logo" href="#">logo</a>
+        <nav>
+          <a id="menu-icon">&#8801;</a>
+        </nav>
+      </header>
+      <header className="hero-header">
+        <h1 className="hero-title">Trova la casa dei tuoi sogni</h1>
+      </header>
+      <footer className="hero-footer">
+        <a className="button-body button-primary" href="#" style={{ borderRadius: "10px" }}>
+          Scopri di più
+        </a>
+      </footer>
+    </section>
+  );
+};
+
+export default Hero;
